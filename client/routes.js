c91388d31f30d7d081e1939e3d1f5cf5aa88b46d
Rowan Griffin <rowangee@gmail.com>
2017-10-28 16:01:17 +0100

    Added subpages

--- client/routes.js
+++ client/routes.js
@@ -2,6 +2,50 @@
 FlowRouter.route('/',{
   name: 'home',
   action(){
-    BlazeLayout.render("homeLayout", {main:"Home"});
+    BlazeLayout.render("layout", {main:"homeLayout"});
+  }
+});
+
+
+//homepage
+FlowRouter.route('/docs',{
+  name: 'docs',
+  action(){
+    BlazeLayout.render("layout", {main:"docsLayout"});
+  }
+});
+
+
+//about
+FlowRouter.route('/about',{
+  name: 'about',
+  action(){
+    BlazeLayout.render("aboutLayout", {main:"About"});
+  }
+});
+
+//forum
+FlowRouter.route('/forum',{
+  name: 'forum',
+  action(){
+    BlazeLayout.render("forumLayout", {main:"Forum"});
+  }
+});
+
+
+//dashboard
+FlowRouter.route('/dash',{
+  name: 'dash',
+  action(){
+    BlazeLayout.render("layout", {main:"dashLayout"});
+  }
+});
+
+//dashboard profile
+FlowRouter.route('/dash/profile',{
+  name: 'dashProfile',
+  action(){
+    BlazeLayout.render("layout", {main:"dashLayout"});
+    BlazeLayout.render("dashLayout", {dashContent:"userProfile"});
   }
 });
