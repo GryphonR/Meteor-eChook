923db8d597318317d160ca0e063e52d5c161b5e9
Rowan Griffin <rowangee@gmail.com>
2017-11-05 10:49:30 +0000

    adding teams

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,9 @@
-Meteor.publish('cars', function(){
-  return Cars.find({author: this.userId})
+Meteor.publish('cars', function() {
+  return Cars.find({
+    author: this.userId
+  })
+});
+
+Meteor.publish('teams', function() {
+  return Teams.find({})
 });
